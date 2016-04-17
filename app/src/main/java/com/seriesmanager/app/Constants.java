eedded9f675c06aed0d7360973a97e00078bc0dd
Guilherme Noya <gui.noya@gmail.com>
2016-04-18 00:05:19 +0100

    Removed data

--- app/src/main/java/com/seriesmanager/app/Constants.java
+++ app/src/main/java/com/seriesmanager/app/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
     public static final String THETVDB_DOMAIN_API = "http://thetvdb.com/api/";
     public static final String TRAKT_DOMAIN_API = "http://api.trakt.tv/";
 
-    public static final String TRAKT_API_KEY = ***REMOVED***;
-    public static final String THETVDB_API_KEY = ***REMOVED***;
+    public static final String TRAKT_API_KEY = *** INSERT API KEY ***;
+    public static final String THETVDB_API_KEY = *** INSERT API KEY ***;
 
     public static final String TRAKT_SHOW_URL = "http://trakt.tv/show/";
 }
